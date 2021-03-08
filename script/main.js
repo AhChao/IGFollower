19309f6656da0d23cded3d47e6613374ae3c8da2
Steven Chao <gash612@gmail.com>
2021-03-09 03:03:47 +0800

    Add basic append function

--- script/main.js
+++ script/main.js
@@ -1,8 +1,36 @@
-let basicQueryLink = "https://www.instagram.com/username/?__a=1";
-//https://www.instagram.com/wikipedia/?__a=1
-/** fetch api url by cors-anywhere */
-/*$.getJSON('https://api.allorigins.win/get?url='
- + encodeURIComponent('https://www.instagram.com/wikipedia/?__a=1'),
-  function (data) { alert(data.contents); });*/
+var globalContainerCount = 0;
+function init()
+{
+	globalContainerCount = 0;
+}
+init();
 
+function LoadInstagramFeed(username,containerId)
+{
+	$.instagramFeed({
+                'username': username,
+                'container': containerId,
+                'display_profile': true,
+                'display_biography': true,
+                'display_gallery': true,
+                'display_captions': true,
+                'max_tries': 8,
+                'callback': null,
+                'styling': true,
+                'items': 8,
+                'items_per_row': 4,
+                'margin': 1,
+                'lazy_load': true,
+                'on_error': console.error
+            });
+}
 
+function newAccount()
+{
+	let account = d3.select("#inputIGAccount").node().value;
+	let containerId = "container" + globalContainerCount;
+	d3.select("#displayField").append("div")
+	.attr("id",containerId)
+	.attr("class",item);
+	LoadInstagramFeed(account,containerId);
+}
