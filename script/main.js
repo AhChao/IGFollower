e7d3b75dea0b4b4b849cc9bd27551c579f8a8fa8
Steven Chao <gash612@gmail.com>
2021-03-09 02:37:20 +0800

    Add lib and basic test

---
+++ script/main.js
@@ -0,0 +1,8 @@
+let basicQueryLink = "https://www.instagram.com/username/?__a=1";
+//https://www.instagram.com/wikipedia/?__a=1
+/** fetch api url by cors-anywhere */
+/*$.getJSON('https://api.allorigins.win/get?url='
+ + encodeURIComponent('https://www.instagram.com/wikipedia/?__a=1'),
+  function (data) { alert(data.contents); });*/
+
+
